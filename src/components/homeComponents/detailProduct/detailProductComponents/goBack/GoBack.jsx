bdfe84cc28f74e9b74c64c8574b7594844c96dca
Armand WADJI <armand_wadji@yahoo.fr>
2022-08-06 19:04:55 +0200

    GoBack style component

--- src/components/homeComponents/detailProduct/detailProductComponents/goBack/GoBack.jsx
+++ src/components/homeComponents/detailProduct/detailProductComponents/goBack/GoBack.jsx
@@ -6,68 +6,34 @@ import {
   View,
 } from "react-native";
 import React from "react";
+import styles from "./GoBackStyle";
 
 const GoBack = () => {
   return (
-    <ScrollView
-      style={{
-        // backgroundColor: "red",
-        marginHorizontal: 20,
-        marginTop: 30,
-      }}>
+    <ScrollView style={[styles.goBackContainer]}>
       {/* GoBack */}
-      <Text
-        style={[
-          styles.title,
-          {
-            textTransform: "capitalize",
-            fontSize: 20,
-            fontWeight: "500",
-            marginBottom: 20,
-          },
-        ]}>
-        retours
-      </Text>
+      <Text style={[styles.title1]}>retours</Text>
 
-      <Text
-        style={[
-          styles.title2,
-          {
-            textTransform: "capitalize",
-            fontSize: 15,
-            fontWeight: "400",
-            letterSpacing: 1,
-            marginBottom: 20,
-          },
-        ]}>
-        Information concernant les retours
-      </Text>
+      <Text style={[styles.title2]}>Information concernant les retours</Text>
 
-      {/* Expedition informations */}
+      {/* GoBack informations */}
       <View style={{ marginBottom: 20 }}>
-        <Text
-          style={{
-            fontWeight: "300",
-            marginBottom: 15,
-          }}>
+        <Text style={[styles.text]}>
           Vous devez nous faire part de votre décision de retourner votre
           commande dans un delais de 30 jours après sa réception.
         </Text>
-        <Text
-          style={{
-            marginBottom: 15,
-            fontWeight: "300",
-          }}>
+        <Text style={[styles.text]}>
           Vous serez totalement remboursé (moins les frais de retour ou
           d'enlèvement), une fois que nous aurons réceptionné l'article.
         </Text>
         <TouchableOpacity>
           <Text
-            style={{
-              fontWeight: "300",
-              marginBottom: 15,
-              textDecorationLine: "underline",
-            }}>
+            style={[
+              styles.text,
+              {
+                textDecorationLine: "underline",
+              },
+            ]}>
             En savoir plus
           </Text>
         </TouchableOpacity>
@@ -77,5 +43,3 @@ const GoBack = () => {
 };
 
 export default GoBack;
-
-const styles = StyleSheet.create({});
---
+++ src/components/homeComponents/detailProduct/detailProductComponents/goBack/GoBackStyle.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  goBackContainer: {
+    // backgroundColor: "red",
+    marginHorizontal: 20,
+    marginTop: 30,
+  },
+  title1: {
+    textTransform: "capitalize",
+    fontSize: 20,
+    fontWeight: "500",
+    marginBottom: 20,
+  },
+  title2: {
+    textTransform: "capitalize",
+    fontSize: 15,
+    fontWeight: "400",
+    letterSpacing: 1,
+    marginBottom: 20,
+  },
+  text: {
+    fontWeight: "300",
+    marginBottom: 15,
+  },
+});
+
+export default styles;
